fc24d6859723fb9ba2e226a814d60cd581c080bb
Xing Meng <mengxingmimi@gmail.com>
2022-04-07 23:52:34 -0700

    added 10 blocks in html, and created a for loop to add color to the textarea according to current hour

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+//identify now time
+//show different color of past,now,and later block
+//save input to local storage
+
+
+//show current date in the header as shown in the example
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+
+// establish a variable to store the current hour
+
+var current_time = new Date($.now());
+var current_hour = parseInt(current_time.getHours())
+
+for (let i = 9 ; i < 18; i ++){
+    id = "#hour-" + i.toString()
+    if (current_hour < i){
+        $(id).addClass('future')
+      }else if (current_hour === i){
+        $(id).addClass('present')
+      }else if (current_hour > i){
+        $(id).addClass('past')
+      }
+}
+
