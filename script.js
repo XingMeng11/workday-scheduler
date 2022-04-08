c9fd4422d25d88f4c0a04c6c4f3b01fab7ff7bab
Xing Meng <mengxingmimi@gmail.com>
2022-04-08 01:13:34 -0700

    use local storage to store input

--- script.js
+++ script.js
@@ -24,3 +24,64 @@ for (let i = 9 ; i < 18; i ++){
       }
 }
 
+//store input into local storage
+
+$('#hour-9').find('button').on('click',function(){
+  var text9 = $('#hour-9').find('textarea').val();
+  localStorage.setItem('text9',text9); 
+})
+
+
+$('#hour-10').find('button').on('click',function(){
+  var text10 = $('#hour-10').find('textarea').val();
+  localStorage.setItem('text10',text10); 
+})
+
+
+$('#hour-11').find('button').on('click',function(){
+  var text11 = $('#hour-11').find('textarea').val();
+  localStorage.setItem('text11',text11); 
+})
+
+
+$('#hour-12').find('button').on('click',function(){
+  var text12 = $('#hour-12').find('textarea').val();
+  localStorage.setItem('text12',text12); 
+})
+
+$('#hour-13').find('button').on('click',function(){
+  var text13 = $('#hour-13').find('textarea').val();
+  localStorage.setItem('text13',text13); 
+})
+
+
+$('#hour-14').find('button').on('click',function(){
+  var text14 = $('#hour-14').find('textarea').val();
+  localStorage.setItem('text14',text14); 
+})
+
+$('#hour-15').find('button').on('click',function(){
+  var text15 = $('#hour-15').find('textarea').val();
+  localStorage.setItem('text15',text15); 
+})
+
+
+$('#hour-16').find('button').on('click',function(){
+  var text16 = $('#hour-16').find('textarea').val();
+  localStorage.setItem('text16',text16); 
+})
+
+$('#hour-17').find('button').on('click',function(){
+  var text17 = $('#hour-17').find('textarea').val();
+  localStorage.setItem('text17',text17); 
+})
+
+
+//restore local storage and display
+
+for (var i = 9; i < 18; i ++){
+  id = "#hour-" + i.toString()
+  text = "text" + i.toString()
+  $(id).find('textarea').val(localStorage.getItem(text));
+}
+
